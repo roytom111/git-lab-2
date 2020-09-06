0d01a4d757e75a981ff5546a72df701d3459fcf2
Roy Frimpong <rf376519@ohio.edu>
2020-09-06 12:12:15 -0400

    initial commit

---
+++ git-lab-program.cc
@@ -0,0 +1,20 @@
+/*
+ *
+ *      File: git-lab-program.cc
+ *      Author:Roy Frimpong 
+ *      Date: 9/3/2020
+ *      Description:
+ */
+
+ #include <iostream>
+ #include <iomanip>
+ #include <cstdlib>
+ using namespace std;
+
+
+ int main(int argc, char const *argv[]){
+
+     cout <<"Hello Git!!"<<endl;
+
+
+ }
